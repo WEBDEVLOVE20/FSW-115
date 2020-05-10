f089b64a674c5a975ec8c50574a4843dc58bead6
WEBDEVLOVE20 <60906754+WEBDEVLOVE20@users.noreply.github.com>
2020-05-10 14:34:09 -0400

    #

--- week5/todo-list/index.js
+++ week5/todo-list/index.js
@@ -1,13 +1,58 @@
+const form = document.getElementById("forms");
+const submits = document.getElementById("submit");
+
+//get requst
 axios.get("https://api.vschool.io/[kelly.compton]/todo")
 .then(response => {
     for (let i = 0; i < response.data.length; i++) {
-    const li = document.createElement("li")
-    li.textContent = response.data[i].title
-    const ul = document.getElementsByTagName("ul")[0]
-    ul.append(li)
-    li.classList = "list"
-    // const button = document.createElement("button")
-    // button.textContent = "delete"
+
+        const h2 = document.createElement("h2")
+        h2.textContent = response.data[i].title
+        document.getElementsByTagName("ul")[0].appendChild(h2)
+
+        const h3 = document.createElement("h3")
+        h3.textContent = "Description: " + response.data[i].description
+        document.getElementsByTagName("ul")[0].appendChild(h3)
+
+        const h4 = document.createElement("h4")
+        h4.textContent = "Price of products: $" + response.data[i].price
+        document.getElementsByTagName("ul")[0].appendChild(h4)
+
+
+    // const li = document.createElement("li")
+    // li.textContent = response.data[i].title
+    // const ul = document.getElementsByTagName("ul")[0]
+    // ul.append(li)
+    //li.classList = "list"
+    
+    //checkbox - put
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.name = "checkbox";
+    checkbox.id = response.data[i]._id;
+    document.getElementsByTagName("ul")[0].appendChild(checkbox)
+    
+    checkbox.addEventListener("click", (event) => {
+        if (checkbox.checked === true) {
+            response.data[i].completed === true
+        
+        axios.put("https://api.vschool.io/[kelly.compton]/todo" + event.target.id)
+
+        .then(response => (response.data))
+        .catch(error => console.log(error))
+        }
+    })
+
+    //line-through
+    if (response.data[i].completed === true) {
+        h2.style.textDecorationLine = "line-through"
+        h3.style.textDecorationLine = "line-through" 
+        h4.style.textDecorationLine = "line-through"  
+}
+
+    //delete button
+// const button = document.createElement("button")
+    // button.textContent = "X"
     // button.id = data[i]._id
     // document.getElementsByTagName("li").appendChild(button)
     // button.addEventListener("click", (event) => {
@@ -15,20 +60,16 @@ axios.get("https://api.vschool.io/[kelly.compton]/todo")
     //     .then(response => console.log(response.data))
     //     .catch(error => console.log())
     // })
-
-    if (response.data[i].completed === true) {
-        li.style.textDecorationLine = "line-through" 
-}
-    document.body.appendChild
+    
+    //document.body.appendChild
 }
 })
 .catch(error => console.log(error))
 
-const form = document.getElementById("forms");
-const submits = document.getElementById("submit");
 
 
-form.addEventListener("submit", function(event){
+// post--new task
+form.addEventListener("submits", function(event){
     event.preventDefault()
 
     const newTodo = {
@@ -42,40 +83,19 @@ form.addEventListener("submit", function(event){
         .catch(error => console.log(error))
 })
 
-// axios.put("https://api.vschool.io/[kelly.compton]/todo" + else.)
 
 
-
-//  .then(response => (response.data))
-//  .catch(error => console.log(error))
-
-    
-//     // LISTS THE TODO TITLES TO THE DOM
-// function listData(response){
-//     // document.getElementById('todo-list').innerHTML = ""
-//     //clearList()
-//     for (let i = 0; i < response.data.length; i++) {
-    
-//     const tasks = document.getElementsByClassName("list")[i]
-//         tasks.textContent = response.data[i].title
-//     if (response.data[i].completed === true) {
-//         tasks.style.textDecorationLine = "line-through" 
-// }
-//     document.body.appendChild
-// }}
+    //clear list
 
     // function clearList(){
-    // const el = document.getElementById('todo-list')
+    // const el = document.getElementById('forms')
     // while(el.firstChild){
     // el.removeChild(el.firstChild)
     // }
     // }
     // getData()
     
-    // // FORM FOR THE POST REQUEST
-    // const todoForm = document["form"]
-    // todoForm.addEventListener("submit", function(e){
-    // e.preventDefault()
+   
 
   
     
